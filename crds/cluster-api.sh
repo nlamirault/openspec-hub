a4a03cf5b7ddd9c63130d2fb4438c0a53dafc510
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2026-03-27 14:58:16 +0100

    feat(crd): add Cluster API CRDs support (#201)
    Add CRD scripts and JSON schemas for Cluster API (CAPI) and Cluster
    API Operator to extend openspec-hub coverage with the following API
    groups: cluster.x-k8s.io, bootstrap.cluster.x-k8s.io,
    controlplane.cluster.x-k8s.io, addons.cluster.x-k8s.io,
    ipam.cluster.x-k8s.io, runtime.cluster.x-k8s.io,
    clusterctl.cluster.x-k8s.io, and operator.cluster.x-k8s.io.
    
    Closes: #186
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ crds/cluster-api.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+export choice=individual
+export FILES=(
+  "addons.cluster.x-k8s.io_clusterresourcesetbindings.yaml"
+  "addons.cluster.x-k8s.io_clusterresourcesets.yaml"
+  "bootstrap.cluster.x-k8s.io_kubeadmconfigs.yaml"
+  "bootstrap.cluster.x-k8s.io_kubeadmconfigtemplates.yaml"
+  "cluster.x-k8s.io_clusterclasses.yaml"
+  "cluster.x-k8s.io_clusters.yaml"
+  "cluster.x-k8s.io_machinedeployments.yaml"
+  "cluster.x-k8s.io_machinedrainrules.yaml"
+  "cluster.x-k8s.io_machinehealthchecks.yaml"
+  "cluster.x-k8s.io_machinepools.yaml"
+  "cluster.x-k8s.io_machines.yaml"
+  "cluster.x-k8s.io_machinesets.yaml"
+  "clusterctl.cluster.x-k8s.io_metadata.yaml"
+  "clusterctl.cluster.x-k8s.io_providers.yaml"
+  "controlplane.cluster.x-k8s.io_kubeadmcontrolplanes.yaml"
+  "controlplane.cluster.x-k8s.io_kubeadmcontrolplanetemplates.yaml"
+  "ipam.cluster.x-k8s.io_ipaddressclaims.yaml"
+  "ipam.cluster.x-k8s.io_ipaddresses.yaml"
+  "runtime.cluster.x-k8s.io_extensionconfigs.yaml"
+)
+
+# renovate: datasource=github-tags depName=kubernetes-sigs/cluster-api
+export VERSION=1.12.4
+
+function generate_url {
+  local crd_file=$1
+  local base_url="https://raw.githubusercontent.com/kubernetes-sigs/cluster-api/refs/tags/v${VERSION}"
+
+  case "${crd_file}" in
+    bootstrap.*)
+      echo "${base_url}/bootstrap/kubeadm/config/crd/bases/${crd_file}"
+      ;;
+    controlplane.*)
+      echo "${base_url}/controlplane/kubeadm/config/crd/bases/${crd_file}"
+      ;;
+    clusterctl.*)
+      echo "${base_url}/cmd/clusterctl/config/crd/bases/${crd_file}"
+      ;;
+    *)
+      echo "${base_url}/config/crd/bases/${crd_file}"
+      ;;
+  esac
+}
